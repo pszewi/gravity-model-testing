ae2536620c0dae02be037ef2667ca5461ae27def
pszewi <jakub.przewoski@gmail.com>
2024-01-09 23:36:54 +0100

    working with a big file

--- code/file_1.py
+++ code/file_1.py
@@ -1,8 +1,10 @@
 import pandas as pd
 
-# idk what you're working in but I would recommend opening this with "display(next(df_gravity))" (only works with set chunksize)
-# because this file is overwhelmingly big  
-# also don't open those files with spreadsheets because this will crash your laptop
-df_gravity = pd.read_csv("../data\Gravity_V202211.csv", index_col=['year'], chunksize=200)
 
-# display(next(df_gravity))
+df_gravity = pd.read_csv("data\Gravity_V202211.csv", index_col=['year'], chunksize=200)
+
+# working with "print(next(df_gravity))" allows you to load the file in 'chunks' (note the 'chunksize' in pd.read_csv)  
+# i.e. every time you copy and paste this command it will print another chunk 
+print(next(df_gravity))
+# print(next(df_gravity)) can see for yourself :)
+# print(next(df_gravity))
