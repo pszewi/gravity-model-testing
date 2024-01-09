28b98bc515a8cca99ee860ce1cab31cda57659c1
pszewi <jakub.przewoski@gmail.com>
2024-01-09 23:24:37 +0100

    descriptions to the file

--- code/file_1.py
+++ code/file_1.py
@@ -1,5 +1,8 @@
 import pandas as pd
 
+# idk what you're working in but I would recommend opening this with "display(next(df_gravity))" (only works with set chunksize)
+# because this file is overwhelmingly big  
+# also don't open those files with spreadsheets because this will crash your laptop
 df_gravity = pd.read_csv("../data\Gravity_V202211.csv", index_col=['year'], chunksize=200)
 
-nums = [1,2,3,4,5,6,7]
+# display(next(df_gravity))
